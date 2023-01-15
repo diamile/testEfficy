2bef131eeea230c98c677e82090787bde39b5294
diamile <diamilesarr2006@gmail.com>
2023-01-15 15:21:25 +0100

    selection des differents elements html au niveau du constructeur.

---
+++ script.js
@@ -0,0 +1,41 @@
+const oppoStatus = [
+  {
+    K_OPPO_STATUS: 1,
+    STATUS: "1. Initial Contact",
+    SUCCESS: 0,
+  },
+  {
+    K_OPPO_STATUS: 2,
+    STATUS: "2. Demonstration",
+    SUCCESS: 25,
+  },
+  {
+    K_OPPO_STATUS: 3,
+    STATUS: "3. Proposal",
+    SUCCESS: 50,
+  },
+  {
+    K_OPPO_STATUS: 4,
+    STATUS: "4. Negotiation",
+    SUCCESS: 75,
+  },
+  {
+    K_OPPO_STATUS: 5,
+    STATUS: "5. Order",
+    SUCCESS: 100,
+  },
+];
+
+const FormComponent = class {
+  constructor() {
+    this.select = document.querySelector("select[name='status']");
+    this.input = document.querySelector("input[name='success']");
+    this.form = document.querySelector("form");
+    this.output = document.querySelector(".output");
+    this.submit = document.querySelector("button[type='submit']");
+  }
+  start() {}
+};
+
+const fc = new FormComponent();
+fc.start();
